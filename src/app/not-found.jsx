7ff328e066f92fdd6ef004a329de387c7db43dbd
razumolla <razumolla75@gmail.com>
2025-12-04 20:00:35 +0600

    404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container mx-auto py-10">
+      <h1>404 - Page not found</h1>
+    </div>
+  );
+};
+
+export default NotFound;

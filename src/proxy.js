cde158192a5657409fd49fbdc61fcf687ed6f13f
razumolla <razumolla75@gmail.com>
2025-12-07 19:09:54 +0600

    proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,15 +1,19 @@
-// src/middleware.js
-
+// proxy.js
 import { NextResponse } from "next/server";
 import { getUserMeLoader } from "@/services/user-me-loader";
 
-export async function middleware(request) {
+export async function proxy(request) {
   const user = await getUserMeLoader();
   const currentPath = request.nextUrl.pathname;
 
   if (currentPath.startsWith("/dashboard") && user.ok === false) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
+
+// Apply proxy only to /dashboard routes
+export const config = {
+  matcher: ["/dashboard", "/dashboard/:path*"],
+};
